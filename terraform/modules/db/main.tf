ce2ca5e580c62dd38846f820ab5f3ebc10f8314d
juliettika <juliettika.timoff@gmail.com>
2022-11-16 00:27:54 +0300

    feat: ansible-3

--- terraform/modules/db/main.tf
+++ terraform/modules/db/main.tf
@@ -1,3 +1,11 @@
+terraform {
+  required_providers {
+    yandex = {
+      source = "yandex-cloud/yandex"
+      version = "0.81.0"
+    }
+  }
+}
 resource "yandex_compute_instance" "db" {
   count = var.instance_count
   name  = "reddit-db-${count.index}"
